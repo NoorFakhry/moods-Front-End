84e89bbd631dc6eecb7ced0d6db9f7506310094b
Noor Fakhry <noor.h.fakhry@gmail.com>
2020-12-02 18:07:14 +0200

    plugged webPlayer

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ import WelcomePage from './UI-components/WelcomePage/WelcomePage';
 import HomePage from './UI-components/HomePage/HomePage';
 import DisplaySearchResults from './UI-components/DisplaySearchResults/DisplaySearchResults';
 import './features/searchResults';
-import './tokens/tokens'
+import './features/webPlayer/webPlayer';
 
 const App = () => {
   return (
     <div className="App">
       <Router>
-        <Route exact path = "/" component = {WelcomePage}></Route>
-        <Route exact path = "/homePage" component = {HomePage}></Route>
-        <Route exact path = "/searchResults" component = {DisplaySearchResults}></Route>
+        <Route exact path = "/" component={WelcomePage}></Route>
+        <Route exact path = "/homePage" component={HomePage}></Route>
+        <Route exact path = "/searchResults" component={DisplaySearchResults}></Route>
       </Router>
     </div>
   );
